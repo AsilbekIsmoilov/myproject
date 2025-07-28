82bdc5d6ae190f983307a1dcdb6681d681364372
Asilbek <asilbekismoilov212002@gmail.com>
2025-07-28 13:51:02 +0500

    lk255 v1

--- project/sheets.py
+++ project/sheets.py
@@ -14,10 +14,24 @@ import hashlib
 import requests
 from django.core.files.base import ContentFile
 import re
-
+from dotenv import load_dotenv
+load_dotenv()
 SCOPE = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-CREDENTIALS = ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\j.khamidullaev\Documents\myproject\credentials.json', SCOPE)
+creds_dict = {
+    "type": os.getenv("GOOGLE_TYPE"),
+    "project_id": os.getenv("GOOGLE_PROJECT_ID"),
+    "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("GOOGLE_PRIVATE_KEY").replace('\\n', '\n'),  
+    "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
+    "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+    "auth_uri": os.getenv("GOOGLE_AUTH_URI"),
+    "token_uri": os.getenv("GOOGLE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("GOOGLE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("GOOGLE_CLIENT_CERT_URL"),
+    "universe_domain": os.getenv("GOOGLE_UNIVERSE_DOMAIN"),
+}
 
+CREDENTIALS = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, SCOPE)
 
 def safe_convert_decimal(value):
     try:
